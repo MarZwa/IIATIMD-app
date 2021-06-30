37577d53fcee046985f8ea0ea429089566e1999a
MarZwa <mgm.zwaan@gmail.com>
2021-06-30 19:13:16 +0200

    Menu aangemaakt

--- app/src/main/java/com/example/fitnesscomrade/MainActivity.java
+++ app/src/main/java/com/example/fitnesscomrade/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.fitnesscomrade;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.bottomNav_view);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_myWorkouts, R.id.navigation_workouts, R.id.navigation_addWorkout)
+                .build();
+
+        NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 }

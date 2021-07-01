4f3df05ab7004ae0f9401e1b7f25d3c3a9179db3
Jim Zweers <JimboZweers@gmail.com>
2021-07-01 16:01:44 +0200

    Kan jwt token terug krijgen

--- app/src/main/java/com/example/fitnesscomrade/workouts.java
+++ app/src/main/java/com/example/fitnesscomrade/workouts.java
@@ -4,61 +4,83 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link workouts#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class workouts extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public workouts() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment workouts.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static workouts newInstance(String param1, String param2) {
-        workouts fragment = new workouts();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private EditText email;
+    private EditText password;
+    private Button button;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_workouts, container, false);
+        View v = inflater.inflate(R.layout.fragment_workouts, container, false);
+
+        email = v.findViewById(R.id.loginEmail);
+        password = v.findViewById(R.id.loginPassword);
+        button = v.findViewById(R.id.loginButton);
+
+        RequestQueue queue = Volley.newRequestQueue(container.getContext());
+        final String URL = "http://10.0.2.2:8000/api/login";
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputEmail = email.getText().toString();
+                String inputPassword =  password.getText().toString();
+
+                if(inputEmail.isEmpty() || inputPassword.isEmpty()){
+
+                }
+
+                else{
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    Log.d("DikkeTieten", response);
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.d("nietGewerkt", error.getMessage());
+                        }
+                    }){
+                        @Override
+                        protected Map<String, String> getParams(){
+                            Map<String, String> params = new HashMap<String, String>();
+                            params.put("email", inputEmail);
+                            params.put("password", inputPassword);
+
+                            return params;
+                        }
+                    };
+                    queue.add(stringRequest);
+                }
+            }
+        });
+
+        return v;
     }
 }
